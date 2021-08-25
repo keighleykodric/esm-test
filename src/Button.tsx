0f6c6d67e3899bf9f6b7ae2a1f22c99781523c3f
keighleykodric <keighleykodric@gmail.com>
2021-08-25 10:51:08 -0700

    dates

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import styles from "./Button.modules.css";
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
 
-export function Button({ title = "Title" }) {
+export function Button({
+  title = formatDistance(subDays(new Date(), 3), new Date(), {
+    addSuffix: true,
+  }),
+}) {
   return <button className={styles.button}>{title}</button>;
 }
